b6eae2d0c96741ae36a2ff5883d6ef8f87cab688
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-19 21:29:58 -0300

    feat: criação do projeto SpringBoot

---
+++ src/main/kotlin/io/github/rayanagoncalves/vendasspringbootexpert/VendasSpringBootExpertApplication.kt
@@ -0,0 +1,11 @@
+package io.github.rayanagoncalves.vendasspringbootexpert
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VendasSpringBootExpertApplication
+
+fun main(args: Array<String>) {
+	runApplication<VendasSpringBootExpertApplication>(*args)
+}
