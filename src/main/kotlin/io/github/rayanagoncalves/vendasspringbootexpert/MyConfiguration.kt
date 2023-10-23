db6bc662103ebbb94778bcf6f6076a0abe0d7b17
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-23 20:55:45 -0300

    feat: trabalhando com múltiplos ambientes e profiles

--- src/main/kotlin/io/github/rayanagoncalves/vendasspringbootexpert/MyConfiguration.kt
+++ src/main/kotlin/io/github/rayanagoncalves/vendasspringbootexpert/MyConfiguration.kt
@@ -1,13 +1,18 @@
 package io.github.rayanagoncalves.vendasspringbootexpert
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("prd")
 class MyConfiguration {
 
-    @Bean(name = ["applicationName"])
-    fun applicationName(): String {
-        return "Sistema de vendas"
+    @Bean
+    fun execute(): CommandLineRunner {
+        return CommandLineRunner {
+            println("Rodando a configuração de produção")
+        }
     }
 }
