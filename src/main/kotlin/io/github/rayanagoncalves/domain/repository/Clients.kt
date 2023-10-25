fe913da035c63030481d7910b45431649109d519
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-25 20:41:27 -0300

    feat: refatorando as outras operações do repositório para usar o JPA

--- src/main/kotlin/io/github/rayanagoncalves/domain/repository/Clients.kt
+++ src/main/kotlin/io/github/rayanagoncalves/domain/repository/Clients.kt
@@ -2,39 +2,44 @@ package io.github.rayanagoncalves.domain.repository
 
 import io.github.rayanagoncalves.domain.entity.Client
 import jakarta.persistence.EntityManager
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
-
-const val INSERT = "insert into client (name) values (?)"
-const val SELECT_ALL = "select * from client"
-const val UPDATE = "update client set name = ? where id = ?"
-const val DELETE = "delete from client where id = ?"
 @Repository
 class Clients(
-    private val jdbcTemplate: JdbcTemplate,
     private val entityManager: EntityManager
 ) {
 
     @Transactional
     fun save(client: Client) = entityManager.persist(client)
 
-    fun update(client: Client) = jdbcTemplate.update(UPDATE, client.name, client.id)
+    @Transactional
+    fun update(client: Client) = entityManager.merge(client)
 
-    fun delete(id: Int) = jdbcTemplate.update(DELETE, id)
+    @Transactional
+    fun delete(client: Client) {
+        if(!entityManager.contains(client)) {
+            entityManager.merge(client)
+        }
+        entityManager.remove(client)
+    }
 
-//    fun getAll(): List<Client> {
-//        return jdbcTemplate.query(SELECT_ALL, getClientMapper())
-//    }
-//    fun findByName(name: String) = jdbcTemplate.query("$SELECT_ALL + WHERE NAME LIKE ?", "%$name%", getClientMapper())
-//
-//    private fun getClientMapper(): RowMapper<Client?>? {
-//        return RowMapper<Client?> { resultSet, i ->
-//            val id = resultSet.getInt("id")
-//            val name = resultSet.getString("nome")
-//            Client(id, name)
-//        }
-//    }
+    @Transactional
+    fun delete(id: Int) {
+        val client = entityManager.find(Client::class.java, id)
+        entityManager.remove(client)
+    }
 
+    @Transactional(readOnly = true)
+    fun findByName(name: String): List<Client> {
+        val jpql = "select c from Client c where c.name like :name"
+        val query = entityManager.createQuery(jpql, Client::class.java)
+        query.setParameter("name", "%$name%")
+        return query.resultList
+    }
+
+    @Transactional(readOnly = true)
+    fun getAll(): List<Client> {
+        return entityManager.createQuery("from Client", Client::class.java).resultList
+    }
 }
