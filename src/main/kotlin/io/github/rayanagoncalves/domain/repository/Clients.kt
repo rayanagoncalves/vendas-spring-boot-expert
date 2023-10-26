414ce937be3ec0c27dade46092896534615509c3
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-10-26 20:56:31 -0300

    feat: trabalhando com @query usando query nativa

--- src/main/kotlin/io/github/rayanagoncalves/domain/repository/Clients.kt
+++ src/main/kotlin/io/github/rayanagoncalves/domain/repository/Clients.kt
@@ -2,14 +2,20 @@ package io.github.rayanagoncalves.domain.repository
 
 import io.github.rayanagoncalves.domain.entity.Client
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
 interface Clients: JpaRepository<Client, Int> {
 
-    @Query(value = "select c from Client c where c.name like :name")
+    @Query(value = "select * from Client c where c.name like '%:name%'", nativeQuery = true)
     fun encontrarPorNome(@Param("name") name: String): List<Client>
+
     fun findByNameLike(name: String): List<Client>
+
+    @Query(value = "delete from Cliente c where c.name = :name")
+    @Modifying
+    fun deleteByName(name: String)
 
     fun existsByName(name: String): Boolean
 }
