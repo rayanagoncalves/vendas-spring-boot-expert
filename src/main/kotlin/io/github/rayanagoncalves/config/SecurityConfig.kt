ac25cea5b1954d1d490ce0e01e868e322276400c
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-11-20 15:52:48 -0300

    feat: adicionando configuração para criptografar a senha

--- src/main/kotlin/io/github/rayanagoncalves/config/SecurityConfig.kt
+++ src/main/kotlin/io/github/rayanagoncalves/config/SecurityConfig.kt
@@ -1,12 +1,20 @@
 package io.github.rayanagoncalves.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @EnableWebSecurity
 class SecurityConfig: WebSecurityConfigurerAdapter() {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 
     override fun configure(auth: AuthenticationManagerBuilder?) { // parte de autenticacao
         super.configure(auth)
