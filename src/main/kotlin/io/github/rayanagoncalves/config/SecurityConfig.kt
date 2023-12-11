2bf58b4fbcf3874cb148bfeaccce20c1c019d8be
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-12-11 20:32:07 -0300

    feat: comenta autenticação

--- src/main/kotlin/io/github/rayanagoncalves/config/SecurityConfig.kt
+++ src/main/kotlin/io/github/rayanagoncalves/config/SecurityConfig.kt
@@ -1,15 +1,15 @@
-package io.github.rayanagoncalves.config
-
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
-
-
-@Configuration
-@EnableWebSecurity
+//package io.github.rayanagoncalves.config
+//
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+//import org.springframework.security.core.userdetails.User
+//import org.springframework.security.core.userdetails.UserDetails
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager
+//
+//
+//@Configuration
+//@EnableWebSecurity
 class SecurityConfig {
 
 //    @Bean
@@ -32,13 +32,13 @@ class SecurityConfig {
 //         //   .permitAll()
 //    }
 
-    @Bean
-    fun userDetailsService(): InMemoryUserDetailsManager? {
-        val user: UserDetails = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("password")
-            .roles("USER")
-            .build()
-        return InMemoryUserDetailsManager(user)
-    }
+//    @Bean
+//    fun userDetailsService(): InMemoryUserDetailsManager? {
+//        val user: UserDetails = User.withDefaultPasswordEncoder()
+//            .username("user")
+//            .password("password")
+//            .roles("USER")
+//            .build()
+//        return InMemoryUserDetailsManager(user)
+//    }
 }
